6f7082fbdf60b52feda35ee4633e3a23d7535549
Katerina Pilatova <kpilatov@redhat.com>
2017-08-03 14:17:07 +0200

    upscaling: fixes

--- playbooks/provisioning/openstack/upscaling_run.sh
+++ playbooks/provisioning/openstack/upscaling_run.sh
@@ -125,5 +125,4 @@ fi
 # - verify the result
 ansible-playbook $ANSIBLE_PARAMS -i "$INVENTORY" \
 $OPENSHIFT_ANSIBLE_CONTRIB/playbooks/provisioning/openstack/upscaling_scale-up.yaml \
--e "$NODES_VAR" -e "openshift_ansible_dir=$OPENSHIFT_ANSIBLE" \
--e "openshift_ansible_contrib_dir=$OPENSHIFT_ANSIBLE_CONTRIB" $VERBOSE
+-e "$NODES_VAR" -e "openshift_ansible_dir=$OPENSHIFT_ANSIBLE" $VERBOSE
